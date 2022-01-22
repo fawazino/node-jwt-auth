62d0ff07f93f180c31aaec0c0d91bf735dd306a6
fawazino <kvngfawaz@gmail.com>
2022-01-22 15:47:13 +0100

    node jwt

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes')
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json())
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://zino:fawaz123@project.imizk.mongodb.net/jwt-Auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000, ()=>{console.log('server running on port 3000');}))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes)
