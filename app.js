85b68644a0cc31239d5040449573e46bad265217
fawazino <hbsinzu@gmail.com>
2022-11-08 17:19:44 +0100

    commit

--- app.js
+++ app.js
@@ -18,14 +18,10 @@ app.use(cookieParser())
 // view engine
 app.set('view engine', 'ejs');
 
-// database connection
-const dbURI = process.env.DB_URI;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000, ()=>{console.log('server running on port', port);}))
-  .catch((err) => console.log(err));
-
 // routes
 app.get('*', checkUser)
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes)
+
+app.listen(3000, ()=>{console.log('server running on port', port);})
